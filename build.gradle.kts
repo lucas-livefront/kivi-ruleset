c85e3d4ee454139f0be73a591af142cdbb63ef67
Lucas Kivi <lucas@livefront.com>
2025-04-23 10:51:59 -0500

    Support publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,25 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     kotlin("jvm") version "2.1.20"
     `maven-publish`
+    signing
 }
 
 group = "org.example.detekt"
 version = "1.0-SNAPSHOT"
+
+/**
+ * Loads local build specified properties.
+ */
+val Project.buildProperties
+    get() = Properties().apply {
+        val buildPropertiesFile = rootProject.file("build.properties")
+        if (buildPropertiesFile.exists()) {
+            FileInputStream(buildPropertiesFile).use { load(it) }
+        }
+    }
 
 dependencies {
     compileOnly("io.gitlab.arturbosch.detekt:detekt-api:1.23.8")
@@ -29,6 +44,47 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
+            pom {
+                name = "kivi-ruleset"
+                description = "Detekt Rules by Kivi"
+                url = "https://github.com/lucas-livefront/kivi-ruleset"
+                inceptionYear = "2025"
+
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        distribution = "repo"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "lucas-livefront"
+                        name = "Lucas"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git://github.com/lucas-livefront/kivi-ruleset.git"
+                    url = "https://github.com/lucas-livefront/kivi-ruleset"
+                }
+            }
         }
     }
+
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = buildProperties.getProperty("sonatype.username")
+                password = buildProperties.getProperty("sonatype.password")
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
 }
